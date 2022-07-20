55e1637c3eca3225dd8543df267ce33922fea287
Josias Nogueira <josias.tamandare@gmail.com>
2022-07-20 19:01:55 -0300

    finalizado exercicio-01

---
+++ fundamentos/bloco-4-Introdução-a-JavaScript-e-Logica-de-Programação/dia-1-javascript-primeiros-passos/exercicio-01.js
@@ -0,0 +1,20 @@
+const a = 10;
+const b = 5;
+
+adição = a + b;
+console.log("A adição de","a","+","b","é",adição);
+
+subtração = a - b;
+console.log("A subitração de","a","-","b","é",subtração);
+
+multiplicação = a * b;
+console.log("A multiplicação de","a","*","b","é",multiplicação);
+
+divisão = a / b;
+console.log("A divisão de","a","/","b","é",divisão);
+
+módulo = a % b;
+console.log("O módulo de","a","%","b","é",módulo);
+
+
+
