c7cb31ef1b21576476d8db593e958a83a981eab8
Josias Nogueira <josias.tamandare@gmail.com>
2022-07-22 19:10:13 -0300

    finalizado exercicio-bonus-02

---
+++ fundamentos/bloco-4-Introdução-a-JavaScript-e-Logica-de-Programação/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus-02.js
@@ -0,0 +1,10 @@
+
+let size = 5;
+let inputLine = "";
+let symbol = "*";
+
+
+for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+    console.log(inputLine);
+    inputLine = inputLine + symbol;
+};
