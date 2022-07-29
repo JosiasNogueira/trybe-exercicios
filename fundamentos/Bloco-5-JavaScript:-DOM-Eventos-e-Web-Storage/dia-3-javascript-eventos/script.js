e3abc2d7dae563ace7444d25955ad34408626683
Josias Nogueira <josias.tamandare@gmail.com>
2022-07-29 19:17:42 -0300

    finalizado exercicio 1

---
+++ fundamentos/Bloco-5-JavaScript:-DOM-Eventos-e-Web-Storage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,21 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo
+//   exercicio 1 A tag <ul> deve conter o id 'days';
+  const lista = document.querySelector('ul');
+  lista.id = 'days';
+
+
