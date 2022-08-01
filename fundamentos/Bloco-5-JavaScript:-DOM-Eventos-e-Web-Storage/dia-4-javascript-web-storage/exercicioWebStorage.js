548101eeb891a88173ad2cf8cfe00d139f18936d
Josias Nogueira <josias.tamandare@gmail.com>
2022-08-01 18:26:39 -0300

    criado botoes

---
+++ fundamentos/Bloco-5-JavaScript:-DOM-Eventos-e-Web-Storage/dia-4-javascript-web-storage/exercicioWebStorage.js
@@ -0,0 +1,8 @@
+let btcBgc = document.getElementById('BackGround-Color');
+let btcTc = document.getElementById('Text-Color');
+let btcFz = document.getElementById('Font-size"');
+let btcLs = document.getElementById('Line-space');
+let btcFf = document.getElementById('Font-family');
+
+
+
