ec16540fb5764c8b198cb456720bda6cf7370d82
Josias Nogueira <josias.tamandare@gmail.com>
2022-08-16 19:54:10 -0300

    finalizado exercicio 1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,1 +1,22 @@
-const sum = require('./sum.js');
+const sum = require('./sum');
+
+describe('exercicio 7.3', () => {
+
+    test('Teste se o retorno de sum(4, 5) é 9', () => {
+        expect(4 + 5).toBe(9);
+    })
+
+    test('Teste se o retorno de sum(0, 0) é 0', () => {
+        expect(0 + 0).toBe(0);
+    })
+
+    test('Teste se a função sum lança um erro quando os parâmetros são 4 e "5"(string 5)', () => {
+        expect(() => sum()).toThrow(Error);
+    })
+
+    test('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada', () => {
+        expect(() => sum()).toThrow("parameters must be numbers");
+    })
+
+
+}) ;
