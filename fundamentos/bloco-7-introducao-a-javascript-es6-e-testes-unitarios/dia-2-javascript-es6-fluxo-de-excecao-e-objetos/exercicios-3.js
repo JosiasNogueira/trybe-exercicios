c83b20639a1487ab0c10277a38949ad24a1a19e2
Josias Nogueira <josias.tamandare@gmail.com>
2022-08-16 08:15:53 -0300

    finalizado exercicico 3 parte 1

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicios-3.js
@@ -0,0 +1,30 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  const adicionandoNoiteLesso2 = (Objecto, key , value) => {
+    Objecto[key] = value; // através do "obj[key]" acessamos a chave do objeto cujo valor queremos modificar. 
+    // A partir daí basta atribuir o valor desejado, que em nossa função é o terceiro parâmetro (value).
+};
+adicionandoNoiteLesso2(lesson2, 'turno', 'noite');
+ console.log(lesson2);
+ 
+  
+
+ 
