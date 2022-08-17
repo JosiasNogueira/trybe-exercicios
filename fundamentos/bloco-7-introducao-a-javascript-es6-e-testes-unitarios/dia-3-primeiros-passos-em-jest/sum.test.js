1988fcb3447d47827df1fbeb670745d5f54f3d36
Josias Nogueira <josias.tamandare@gmail.com>
2022-08-17 08:48:30 -0300

    finalizado exercicio 3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,4 @@
-const { sum, myRemove } = require('./sum');
+const { sum, myRemove, myFizzBuzz } = require('./sum');
 
 describe('exercicio 7.3 exercicio 1', () => {
 
@@ -37,5 +37,26 @@ describe('exercicio 7.3 exercicio 2', () => {
 
 });
 
-describe('exercicio 7.3 exercicio 2', () => {
+describe('exercicio 7.3 exercicio 3', () => {
+
+    test('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(15)).toBe('fizzbuzz');
+    })
+
+    test('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(9)).toBe('fizz');
+    })
+
+    test('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(5)).toBe('buzz');
+    })
+
+    test('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(1)).toBe(1);
+    })
+
+    test('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz('ola')).toBe(false);
+    })
+
 });
