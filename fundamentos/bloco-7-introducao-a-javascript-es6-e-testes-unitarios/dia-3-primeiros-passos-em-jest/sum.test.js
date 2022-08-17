26a9b9beb63609de18a51df4b831631baf6d7fb8
Josias Nogueira <josias.tamandare@gmail.com>
2022-08-17 08:11:26 -0300

    finalizado exercicio 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -27,4 +27,15 @@ describe('exercicio 7.3 exercicio 2', () => {
         expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
     })
 
+    test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    })
+
+    test('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    })
+
+});
+
+describe('exercicio 7.3 exercicio 2', () => {
 });
