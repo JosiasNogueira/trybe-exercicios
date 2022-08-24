7ed8af58a683cddb1161cf5b3886821ae0a91448
Josias Nogueira <josias.tamandare@gmail.com>
2022-08-24 19:36:16 -0300

    Finalizado exercicio 4

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise4.js
@@ -0,0 +1,71 @@
+const books = [
+    {
+        id: 1,
+        name: 'As Crônicas de Gelo e Fogo',
+        genre: 'Fantasia',
+        author: {
+            name: 'George R. R. Martin',
+            birthYear: 1948,
+        },
+        releaseYear: 1991,
+    },
+    {
+        id: 2,
+        name: 'O Senhor dos Anéis',
+        genre: 'Fantasia',
+        author: {
+            name: 'J. R. R. Tolkien',
+            birthYear: 1892,
+        },
+        releaseYear: 1954,
+    },
+    {
+        id: 3,
+        name: 'Fundação',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Isaac Asimov',
+            birthYear: 1920,
+        },
+        releaseYear: 1951,
+    },
+    {
+        id: 4,
+        name: 'Duna',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Frank Herbert',
+            birthYear: 1920,
+        },
+        releaseYear: 1965,
+    },
+    {
+        id: 5,
+        name: 'A Coisa',
+        genre: 'Terror',
+        author: {
+            name: 'Stephen King',
+            birthYear: 1947,
+        },
+        releaseYear: 1986,
+    },
+    {
+        id: 6,
+        name: 'O Chamado de Cthulhu',
+        genre: 'Terror',
+        author: {
+            name: 'H. P. Lovecraft',
+            birthYear: 1890,
+        },
+        releaseYear: 1928,
+    },
+];
+
+function longestNamedBook() { //Começamos criando uma estrutura básica de um reduce. O acumulador não precisa de um valor inicial, dessa forma ele assume o valor do primeiro item do array
+    return books.reduce((biggestBook, currentBook) => {
+        if (currentBook.name.length > biggestBook.name.length) { //Checamos se o livro atual tem nome maior que o presente no acumulador, senão mantemos o mesmo.
+            return currentBook;
+        }
+        return biggestBook;
+    });
+}
