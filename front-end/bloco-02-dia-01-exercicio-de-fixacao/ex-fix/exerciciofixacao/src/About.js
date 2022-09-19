29bfb01f760f97fd0ef245579e34d41bf9bf1f5e
Josias Nogueira <josias.tamandare@gmail.com>
2022-09-19 19:36:13 -0300

    Finalizado exer

---
+++ front-end/bloco-02-dia-01-exercicio-de-fixacao/ex-fix/exerciciofixacao/src/About.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          {/Josias/}
+        </h1>
+        <p>
+          {/Sou um cara legal!/}
+        </p>
+        <h2>Minhas Abilidades</h2>
+        <ul>
+          <li>/ Minha Habilidade 01 /</li>
+          <li>/ Minha Habilidade 02 /</li>
+          <li>/ Minha Habilidade 03 /</li>
+          <li>/ Minha Habilidade 04 /</li>
+
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
